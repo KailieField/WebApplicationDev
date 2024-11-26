7890b0647a5603efe49663694f0b55538306002a
Kailie Field <kailie.field@gmail.com>
2024-11-26 01:45:49 -0500

    updating order-service, inventory-service

---
+++ microservice-parent/inventory-service/src/main/java/ca/gbc/inventoryservice/model/Inventory.java
@@ -0,0 +1,28 @@
+package ca.gbc.inventoryservice.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Inventory {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name="skuCode")
+	private String skuCode;
+
+	@Column(name="quantity")
+	private Integer quantity;
+}
